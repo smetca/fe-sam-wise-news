f78ef15568e089c950b83702a5c77c9a6c932e14
smetca <s.metcalfe.h@gmail.com>
2019-10-07 16:19:43 +0100

    Added styling and paragraph formatting to ArticleShowcase and added footer styles module

--- src/components/ArticleShowcase.jsx
+++ src/components/ArticleShowcase.jsx
@@ -9,7 +9,9 @@ class ArticleShowcase extends Component {
       author: "weegembump",
       article_id: 1,
       votes: 20,
-      body: "Text from the article is here and I must type enough to fill the ellipses lorem ipsum dolor blah blah blah something something something really",
+      body: `Text from the article is here and I must type enough to fill the ellipses lorem ipsum dolor blah blah blah something something something really. This is a sentence of some sort. Keep reading until the ellipses
+        The dog crossed the road with his owner, and something happened. Look at that in the distance something something
+        lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor words words words words`,
       created_at: 1527695953341,
       comment_count: 100
     }
@@ -19,9 +21,27 @@ class ArticleShowcase extends Component {
     const { article } = this.state;
     return (
       <section className={styles.showcase}>
+        <h2>Top Article</h2>
         <article>
-          <h2>{article.title}</h2>
-          <p>{article.body.slice(0, 100) + '...'}</p>
+          <h3>{article.title}</h3>
+          {
+            article.body.length >= 300
+              ? article.body
+                .slice(0,300)
+                .split('\n')
+                .map((paragraph, index, array) => {
+                  if(index < array.length - 1) {
+                    return <p>{paragraph}</p>
+                  } else {
+                    return <p>{paragraph}...</p>
+                  }
+                })
+              : article.body
+                  .split('\n')
+                  .map(paragraph => {
+                    return <p>{paragraph}</p>
+                  })
+          }
         </article>
         <button>
           View All Articles
